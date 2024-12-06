2c1247603147f2f30ace1d1a4ae0f0255ec6f0de
unknown <salehelmagdy5@gmail.com>
2024-12-06 21:32:05 +0200

    fix no sessin after otp login

--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -98,23 +98,19 @@ export const verifySecret = async ({
 };
 
 export const getCurrentUser = async () => {
-  try {
-    const { databases, account } = await createSessionClient();
+  const { databases, account } = await createSessionClient();
 
-    const result = await account.get();
+  const result = await account.get();
 
-    const user = await databases.listDocuments(
-      appwriteConfig.databaseId,
-      appwriteConfig.usersCollectionId,
-      [Query.equal("accountId", result.$id)]
-    );
+  const user = await databases.listDocuments(
+    appwriteConfig.databaseId,
+    appwriteConfig.usersCollectionId,
+    [Query.equal("accountId", result.$id)]
+  );
 
-    if (user.total <= 0) return null;
+  if (user.total <= 0) return null;
 
-    return parseStringify(user.documents[0]);
-  } catch (error) {
-    console.log(error);
-  }
+  return parseStringify(user.documents[0]);
 };
 
 export const signOutUser = async () => {
